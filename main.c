79bc80a9468f8cecc6463c7c6f1bdb4a2c052717
rvieira1001 <rvieira.almeida1001@gmail.com>
2026-04-01 16:38:34 -0300

    corrige segfault no espanhol

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 int main(int argc, char* argv[]) {
     if ((argc > 2) && (strcmp(argv[2], "pt-br") == 0))
         printf("\nOlá, %s!", argv[1]);/* adiciondo pt br? */
-    else if (strcmp(argv[2], "es") == 0)
+    else if ((argc > 2) && (strcmp(argv[2], "es") == 0))
     	printf("\nHola, %s!", argv[1]);/* adicionando espanhol */
     else
         printf("\nHello, %s!", argv[1]);
